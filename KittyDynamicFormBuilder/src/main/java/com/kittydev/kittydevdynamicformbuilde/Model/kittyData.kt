c4987502d2824aa9e9c18b890256f8b9cc4f1bee
padmadev <padmadev@kitty-dev.com>
2021-05-19 19:37:29 +0530

    error recover

---
+++ KittyDynamicFormBuilder/src/main/java/com/kittydev/kittydevdynamicformbuilde/Model/kittyData.kt
@@ -0,0 +1,24 @@
+package com.kittydev.kittydevdynamicformbuilde.Model
+
+
+import android.widget.LinearLayout
+import androidx.lifecycle.MutableLiveData
+import com.kittydev.kittydevdynamicformbuilde.KittyElements
+
+data class attributeDM(
+    val tag: String,
+    val type: KittyElements.Type,
+    val hint: String? = null,
+    val value: String? = null,
+    val heading: String? = null,
+    val subHeading: String? = null,
+    val options: List<String>? = listOf(),
+    var selectedOptions: List<String>? = listOf(),
+    var isRequired: Boolean = false,
+    var isRefreshBtn: Boolean = false,
+    var isEnabled: Boolean = true,
+    val params: LinearLayout.LayoutParams? = null,
+    val valueListener: MutableLiveData<String>? = null,
+    val refreshListener: MutableLiveData<String>? = null,
+    val drawable: Int ?=0
+)
